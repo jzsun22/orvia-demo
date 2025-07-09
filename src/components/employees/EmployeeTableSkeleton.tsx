9f991671b70583a9a213e28efa8c80e911ba998b
Jocelyn S. <jocelyn.z.sun@gmail.com>
2025-07-09 01:10:39 -0700

    added animations

--- src/components/employees/EmployeeTableSkeleton.tsx
+++ src/components/employees/EmployeeTableSkeleton.tsx
@@ -12,31 +12,31 @@ const EmployeeTableSkeleton = () => {
                 scope="col"
                 className="py-4 pl-8 text-left text-sm font-medium text-charcoalcocoa uppercase tracking-wider"
               >
-                <Skeleton className="h-4 w-20" />
+                <Skeleton className="h-4 w-20 animate-pulse duration-1200" />
               </th>
               <th
                 scope="col"
                 className="py-4 pr-4 text-left text-sm font-medium text-charcoalcocoa uppercase tracking-wider hidden lg:table-cell xl:hidden"
               >
-                <Skeleton className="h-4 w-40" />
+                <Skeleton className="h-4 w-40 animate-pulse duration-1200" />
               </th>
               <th
                 scope="col"
                 className="py-4 px-0 text-left text-sm font-medium text-charcoalcocoa uppercase tracking-wider hidden xl:table-cell"
               >
-                <Skeleton className="h-4 w-24" />
+                <Skeleton className="h-4 w-24 animate-pulse duration-1200" />
               </th>
               <th
                 scope="col"
                 className="py-4 px-2 text-left text-sm font-medium text-charcoalcocoa uppercase tracking-wider hidden xl:table-cell"
               >
-                <Skeleton className="h-4 w-32" />
+                <Skeleton className="h-4 w-32 animate-pulse duration-1200" />
               </th>
               <th
                 scope="col"
                 className="py-4 pr-6 text-left text-sm font-medium text-charcoalcocoa uppercase tracking-wider"
               >
-                <Skeleton className="h-4 w-20" />
+                <Skeleton className="h-4 w-20 animate-pulse duration-1200" />
               </th>
             </tr>
           </thead>
@@ -44,21 +44,21 @@ const EmployeeTableSkeleton = () => {
             {Array.from({ length: 16 }).map((_, i) => (
               <tr key={i}>
                 <td className="py-6 pl-8 pr-4">
-                  <Skeleton className="h-5 w-24 xl:w-40" />
+                  <Skeleton className="h-5 w-24 xl:w-40 animate-pulse duration-1200" />
                 </td>
                 <td className="py-6 pr-4 hidden lg:table-cell xl:hidden">
-                  <Skeleton className="h-5 xl:w-64" />
+                  <Skeleton className="h-5 xl:w-64 animate-pulse duration-1200" />
                 </td>
                 <td className="py-6 px-2 hidden xl:table-cell">
-                  <Skeleton className="h-5 w-48" />
+                  <Skeleton className="h-5 w-48 animate-pulse duration-1200" />
                 </td>
                 <td className="py-6 px-2 hidden xl:table-cell">
-                  <Skeleton className="h-5 w-32" />
+                  <Skeleton className="h-5 w-32 animate-pulse duration-1200" />
                 </td>
                 <td className="py-6 pr-6">
                   <div className="flex gap-2">
-                    <Skeleton className="h-8 w-20 rounded-md" />
-                    <Skeleton className="h-8 w-32 rounded-md" />
+                    <Skeleton className="h-8 w-20 rounded-md animate-pulse duration-1200" />
+                    <Skeleton className="h-8 w-32 rounded-md animate-pulse duration-1200" />
                   </div>
                 </td>
               </tr>
